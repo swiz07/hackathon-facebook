ebc933456a0b6cf3b35e7443b1d7d677fe773f34
Swizel De Melo <swizeldemelo@gmail.com>
2024-12-14 22:02:11 +0000

    Added toggle for like button

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-
+const likeButton= document.getElementsByClassName("like-btn");
 function bgChange(x){
     x.style.backgroundColor="pink";
 }
 
 function normalbgColor(x){
     x.style.backgroundColor="#20E3D9";
+}
+
+function toggleLike(likeButton){
+    if(likeButton.innerHTML==="Like"){
+        likeButton.innerHTML="Unlike";
+        likeButton.style.backgroundColor = "rgba(238, 105, 127, 0.84)";
+        likeButton.style.borderColor = "yellow";
+    }
+    else{
+        likeButton.innerHTML="Like";
+        likeButton.style.backgroundColor = "red";
+        likeButton.style.borderColor = "yellow";
+    }
 }
